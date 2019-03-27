8d5d507ff2b2b3dd9d7d320e2267a80a7558a3d9
Codey Pinero <codey.pinero@gmail.com>
2019-03-27 21:31:42 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,41 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |prize_winner|
+    if prize_winner["status"] == "Winner"
+      return prize_winner["name"].split(' ').first
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, info|
+    info.each do |prize_winner|
+      if prize_winner["occupation"] == occupation
+        return prize_winner["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, info|
+    info.each { |contestant| count += 1 if contestant["hometown"] == hometown }
+  end
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, info|
+    info.each { |contestant| return contestant["occupation"] if contestant["hometown"] == hometown }
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  total_age = 0
+  data[season].each { |contestant| total_age += contestant["age"].to_i }
+
+  average = total_age.to_f/data[season].length
+  average.round
 end
